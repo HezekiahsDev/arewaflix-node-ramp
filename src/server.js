cbcd6fbe971a05cabb4cb12b0eec075be4c51a11
hezekiahs-swe <hezekiahs.dev@gmail.com>
2025-10-21 17:55:20 +0100

    hezekiahs-dev: change port - 2025-10-21 17:55:20

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./app.js";
 
 // The API is now mounted at /api/v1 (see app.js and routes/index.js)
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
